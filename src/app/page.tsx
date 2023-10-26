7e9c331cf02e939793cf7a53ae4fdbd38a113ea6
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-10-26 13:26:39 -0300

    feat: refactoring pages of project

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Login() {
-  return (
-    <div className='flex flex-col min-h-screen bg-primary justify-center items-center'>
-      <Image src='/white-logo.svg' alt='Simple Social logo' width={200} height={200}/>
-    </div>
-  )
-}
