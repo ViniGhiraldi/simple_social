80d54cfee6ab8361a53b0ed956f6510ed208a57a
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-11-29 13:28:49 -0300

    feat: login screen restructuring

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,16 +4,16 @@ import { LoginForm } from "./components/login-form";
 
 export default async function Login() {
   return (
-    <div className='flex flex-col min-h-screen bg-primary justify-center items-center gap-6'>
-      <Image src='/white-logo.svg' alt='Simple Social logo' width={200} height={200} className=''/>
-      <Card className="w-80">
-        <CardHeader>
-          <h1 className="text-2xl font-bold">Entrar</h1>
-        </CardHeader>
-        <CardContent>
+    <div className="min-h-screen flex">
+      <div className="flex-1 bg-primary flex items-center justify-center">
+        <Image src='/white-logo.svg' alt='Simple Social logo' width={250} height={250} className=''/>
+      </div>
+      <div className="flex-1 bg-secondary flex items-center justify-center">
+        <div className="w-1/2 space-y-6 rounded-xl border-2 p-6">
+          <h1 className="text-4xl">Entrar</h1>
           <LoginForm/>
-        </CardContent>
-      </Card>
+        </div>
+      </div>
     </div>
   )
 }
